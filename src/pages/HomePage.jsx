14dc77b00b3903b74f2aca20617cfa4d9ed69e04
Safin-Echologyx <safin@echologyx.com>
2025-01-28 15:10:39 +0600

    Features:  1. Registration Form Created  2. Registration Form Validation done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,51 @@
-import { Link } from "react-router-dom";
+import { useReducer, useEffect } from "react";
+import { postReducer, initialState } from "../reducers/PostReducer";
+import { useAxios } from "../hooks/useAxios";
+import PostList from "../components/posts/PostList";
+import { actions } from "../actions";
 
-export default function HomePage() {
+const HomePage = () => {
+  const [state, dispatch] = useReducer(postReducer, initialState);
+  const { api } = useAxios();
+
+  useEffect(() => {
+    dispatch({ type: actions.post.DATA_FETCHING });
+    const fetchPost = async () => {
+      try {
+        const response = await api.get(
+          `${import.meta.env.VITE_SERVER_BASE_URL}/posts`
+        );
+
+        console.log(response)
+        if (response.status === 200) {
+          dispatch({
+            type: actions.post.DATA_FETCHED,
+            data: response.data
+          });
+        }
+      } catch (error) {
+        dispatch({
+          type: actions.post.DATA_FETCH_ERROR,
+          error: error.message,
+        });
+      }
+    };
+    fetchPost()
+  }, []);
+
+  if (state?.loading) {
+    return <div> We are working...</div>;
+  }
+
+  if (state?.error) {
+    return <div> Error in fatching posts {state?.error?.message}</div>;
+  }
 
   return (
     <div>
-      <p>Home page</p>
-      <Link to='/me'> Profile</Link>
+      <PostList posts={state?.posts} />
     </div>
   );
-}
+};
+
+export default HomePage;
--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,7 +1,41 @@
+import { Link } from "react-router-dom";
+import RegIcon from "../assets/images/auth_illustration.png";
+import RegistrationForm from "../components/auth/RegistrationForm";
 export default function RegistrationPage() {
   return (
-    <div>
-      
-    </div>
-  )
+    <main className="flex items-center justify-center min-h-screen py-8 bg-deepDark">
+      <div className="max-w-[1368px] flex-1">
+        <div className="container grid items-center gap-8 lg:grid-cols-2">
+          <div>
+            <img className="mb-12 h-60" src={RegIcon} alt="auth_illustration" />
+            <div>
+              <h1 className="mb-3 text-4xl font-bold lg:text-[40px]">
+                Facehook
+              </h1>
+              <p className="max-w-[452px] text-gray-400/95 lg:text-lg">
+                Create a social media app with features like, showing the post,
+                post details, reactions, comments and profile.
+              </p>
+            </div>
+          </div>
+
+          <div className="card">
+            <RegistrationForm/>
+            <div className="py-4 lg:py-4">
+              <p className="text-xs text-center text-gray-600/95 lg:text-sm">
+                Already have an account?
+                <Link
+                  className="text-white transition-all hover:text-lwsGreen hover:underline"
+                  to="/login"
+                >
+                  {" "}
+                  Login
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
